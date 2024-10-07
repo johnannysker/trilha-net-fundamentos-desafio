7b1ca7867a663d67b02ddb7fefdb99043397e8a8
johnannysker <john-ao@hotmail.com>
2024-10-07 19:56:29 -0300

    Exclusão dos lembretes TODO

--- DesafioFundamentos/Models/Estacionamento.cs
+++ DesafioFundamentos/Models/Estacionamento.cs
@@ -35,14 +35,12 @@ namespace DesafioFundamentos.Models
             {
                 Console.WriteLine("Digite a quantidade de horas que o veículo permaneceu estacionado:");
 
-                // TODO: Pedir para o usuário digitar a quantidade de horas que o veículo permaneceu estacionado, 
-                // TODO: Realizar o seguinte cálculo: "precoInicial + precoPorHora * horas" para a variável valorTotal                
                 // *IMPLEMENTE AQUI*
                 Console.WriteLine();
                 int horas = int.Parse(Console.ReadLine());
                 decimal valorTotal = precoInicial + (horas * precoPorHora);
 
-                // TODO: Remover a placa digitada da lista de veículos
+
                 // *IMPLEMENTE AQUI*
                 veiculos.Remove(placa);
                 Console.WriteLine($"O veículo de placa: {placa} foi removido e o preço total foi de: R$ {valorTotal.ToString("F2")}");
@@ -60,7 +58,6 @@ namespace DesafioFundamentos.Models
             {
 
                 Console.WriteLine("*** Veiculos estacionados ***");
-                // TODO: Realizar um laço de repetição, exibindo os veículos estacionados
                 // *IMPLEMENTE AQUI*
                 foreach (string placa in veiculos)
                 {
